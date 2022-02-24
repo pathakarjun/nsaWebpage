47687ff62860b535165fdaaa33740fc20d7affd7
Arjun <pathakarjun2010@gmail.com>
2022-02-23 21:34:01 -0600

    Created Navgation Bar

---
+++ frontend/src/Blog/BlogHeader.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class BlogHeader extends Component {
+  render() {
+    return (
+      <div className="h-60 bg-primary">
+        <h1 className="text-6xl font-bold text-white text-center">BLOGs</h1>
+      </div>
+    );
+  }
+}
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,7 +1,26 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "870px" },
+      // => @media (max-width: 799px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+    },
+    extend: {
+      colors: {
+        primary: "#FF7C51",
+        primaryb: "#242222",
+        secondary: "#424141",
+      },
+    },
   },
   plugins: [],
 };
