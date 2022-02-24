732c3c2aebc647f17c12f81824b7002a6e5a74a6
Arjun <pathakarjun2010@gmail.com>
2022-02-24 00:00:27 -0600

    Created create blog button on navigation bar

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
       xl: { max: "1279px" },
       // => @media (max-width: 1279px) { ... }
 
-      lg: { max: "1023px" },
+      lg: { min: "870px" },
       // => @media (max-width: 1023px) { ... }
 
       md: { max: "870px" },
