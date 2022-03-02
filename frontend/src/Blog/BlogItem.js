8414f187cc6d09172bba2263bbbdf3c33c7de0dd
Arjun <pathakarjun2010@gmail.com>
2022-03-01 23:03:45 -0600

    Added blog writter name and chageed blog layout

--- frontend/src/Blog/BlogItem.js
+++ frontend/src/Blog/BlogItem.js
@@ -3,33 +3,42 @@ import { Link } from "react-router-dom";
 
 export class BlogItem extends Component {
   render() {
-    const { title, excerpt, date, id } = this.props.blog;
+    const { title, excerpt, date, id, acf } = this.props.blog;
 
     var rDate = new Date(date);
+    const imgUrl = `https://robohash.org/${rDate}`;
     return (
-      <div className=" relative w-[21rem] h-[27rem] overflow-hidden transition-shadow duration-300 bg-white rounded-lg shadow-lg">
+      <div className=" relative w-[21rem] h-[23rem] overflow-hidden transition-shadow duration-300 bg-white rounded-lg shadow-2xl">
         <Link to={`/blogContent/${id}`}>
-          <img
-            src="https://images.pexels.com/photos/2408666/pexels-photo-2408666.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
-            className="object-cover w-full h-28"
-            alt=""
-          />
+          <div className="object-cover w-full h-4 bg-blue-600" alt="" />
           <div className="p-6 ">
-            <p className="mb-3 text-xs font-semibold tracking-wide uppercase">
-              <span className="text-gray-600">{rDate.toDateString()}</span>
-            </p>
-            <p
-              dangerouslySetInnerHTML={{ __html: title.rendered }}
-              className="inline-block py-0 text-xl font-bold  transition-colors duration-200 font-sans line-clamp-3 leading-[1.58]"
-            />
+            <div className="h-[15rem] overflow-hidden ">
+              <p
+                dangerouslySetInnerHTML={{ __html: title.rendered }}
+                className="inline-block py-0 text-xl font-bold  transition-colors duration-200 font-sans line-clamp-3 leading-[1.58]"
+              />
 
-            <p
-              className="mb-2 text-gray-600 text-sm text-justify py-2 line-clamp-4 leading-7"
-              dangerouslySetInnerHTML={{ __html: excerpt.rendered }}
-            />
-            <p className="absolute bottom-5 font-semibold transition-colors duration-200 hover:text-deep-purple-800 text-left text-inherit">
-              Read more
-            </p>
+              <p
+                className="mb-2 text-gray-600 text-sm py-3 leading-7 whitespace-normal"
+                dangerouslySetInnerHTML={{ __html: excerpt.rendered }}
+              />
+            </div>
+
+            <hr className="border-b-1 border-gray-200  mx-4 my-4" />
+
+            <div className="flex w-full items-center font-sans absolute bottom-4 bg-white">
+              <img
+                className=" w-12 h-12 rounded-full mr-2 -ml-2"
+                src={imgUrl}
+                alt="Avatar of Author"
+              />
+              <div className="flex-1 mt-4">
+                <p className=" text-sm font-medium  leading-none mb-2">
+                  {acf.author}
+                </p>
+                <p className="text-gray-500 text-xs ">{rDate.toDateString()}</p>
+              </div>
+            </div>
           </div>
         </Link>
       </div>
