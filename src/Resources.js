2e5e606b19578ff17020d67fbfe7a7fec937ab80
Arjun <pathakarjun2010@gmail.com>
2022-03-11 19:44:31 -0600

    small ui changes to look better

--- src/Resources.js
+++ src/Resources.js
@@ -19,7 +19,7 @@ export default function Resources() {
   return (
     <div>
       {isLoaded ? (
-        <div className="container w-full mx-auto pt-20 mb-40 px-6">
+        <div className="container w-full mx-auto pt-20 mb-40 px-6 md:px-2">
           {resources.map((resources) => (
             <FaqCard key={resources.id} data={resources} />
           ))}
